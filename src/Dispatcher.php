3c5b7290eb53bb0bc1e589a3842039a2470e3852
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-27 00:49:14 -0300

    Irá executar o controller ou método correspondente passado na rota

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -4,7 +4,28 @@
 
     class Dispatcher
     {
-        
+        public function dispatch($callback, $params = [], $namespace = "App\\")
+        {
+            if (is_callable($callback)) {
+                return call_user_func_array($callback, array_values($params));
+            } else if (is_string($callback)) {
+                if (!!strpos($callback, '@') != false) {
+                    $callback = explode('@', $callback);
+                    $controller = $namespace . $callback[0];
+                    $method = $callback[1];
+
+                    $rc = new \ReflectionClass($controller);
+
+                    if ($rc->isInstantiable() && $rc->hasMethod($method)) {
+                        return call_user_func_array(array(new $controller, $method), array_values($params));
+                    } else {
+                        throw new \Exception("Erro ao despachar: controller não pode ser instanciado ou método não exite");
+                    }
+                }
+            } else {
+                throw new \Exception("Erro ao despachar: método não implementado");
+            }
+        }
     }
 
 ?>
