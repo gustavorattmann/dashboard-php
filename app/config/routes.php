0716229a6047efe7a0a825b0f2d7b9aa7dc37e60
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-27 14:43:46 -0300

    Adicionado novas rotas para teste

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,16 @@
 <?php
 
-    $router->get('/', function(){
+    $router->get('/', function() {
         echo "Página inicial";
     });
     
-    $router->get('/contatos', function(){
+    $router->get('/contatos', function() {
         echo "Página de contatos";
+        die();
+    });
+
+    $router->get('/teste/{teste}', function($teste) {
+        echo "Agora foi recebido da URI o parâmetro: " . $teste;
     });
     
     $router->post('/contatos/store', "Controller@store");
