a0bedbd606b9fd3120033269fa584e42b7cee619
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-27 00:46:24 -0300

    Rotas para teste

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,13 @@
 <?php
 
-
+    $router->get('/', function(){
+        echo "Página inicial";
+    });
+    
+    $router->get('/contatos', function(){
+        echo "Página de contatos";
+    });
+    
+    $router->post('/contatos/store', "Controller@store");
 
 ?>
