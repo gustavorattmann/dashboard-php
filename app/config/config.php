ef3e51050cfc428617e739c93b3925b4b53782b7
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-26 23:33:01 -0300

    Configuração dos diretórios e banco de dados

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,12 @@
 <?php
 
+    # Definição dos diretórios
     define('DS', DIRECTORY_SEPARATOR);
-    define('BASE', DS);
+    define('BASE_PATH', dirname(dirname(__DIR__)) . DS);
+    define('APP_PATH', BASE_PATH . 'app' . DS);
+    define('SRC_PATH', BASE_PATH . 'src' . DS);
+    define('CONFIG_PATH', APP_PATH . 'config' . DS);
+
+    # Configuração do banco de dados
 
 ?>
