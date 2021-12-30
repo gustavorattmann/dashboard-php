906aceddb01ce6bdc8ec3b8f0e86b5840b46e954
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-30 04:24:30 -0300

    Adicionado caminho da pasta view

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,7 @@
     define('APP_PATH', BASE_PATH . 'app' . DS);
     define('SRC_PATH', BASE_PATH . 'src' . DS);
     define('CONFIG_PATH', APP_PATH . 'config' . DS);
+    define('VIEW_PATH', APP_PATH . 'view' . DS);
 
     # Configuração do banco de dados
 
