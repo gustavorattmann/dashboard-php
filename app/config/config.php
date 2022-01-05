263e971cd7fe365201c1c88d3f6d1fadc397311f
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-01-05 02:30:01 -0300

    Adicionado configurações do banco de dados

--- app/config/config.php
+++ app/config/config.php
@@ -9,5 +9,11 @@
     define('VIEW_PATH', APP_PATH . 'view' . DS);
 
     # Configuração do banco de dados
+    define('DB_DRIVER', $_ENV['DB_DRIVER'] ?? 'mysql');
+    define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
+    define('DB_PORT', $_ENV['DB_PORT'] ?? '3306');
+    define('DB_USER', $_ENV['DB_USER'] ?? 'root');
+    define('DB_PASSWORD', $_ENV['DB_PASSWORD'] ?? '');
+    define('DB_DATABASE', $_ENV['DB_DATABASE'] ?? 'dashboard');
 
 ?>
