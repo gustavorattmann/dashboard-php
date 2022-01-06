fab9dc68d9926b641dd7ac59a5e3096790460ed6
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-01-06 01:14:10 -0300

    Resetado classe

--- core/Controller.php
+++ core/Controller.php
@@ -4,20 +4,7 @@
 
     class Controller
     {
-        protected function render(string $view, $data = [])
-        {
-            if (strpos($view, 'partials') === false) {
-                $content = 'pages' . DS;
-            } else {
-                $content = '';
-            }
-            
-            $loader = new \Twig\Loader\FilesystemLoader(VIEW_PATH);
-
-            $twig = new \Twig\Environment($loader);
-
-            echo $twig->render($content . $view . '.twig.php', $data);
-        }
+        
     }
 
 ?>
